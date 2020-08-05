8f513ae43e7b8160d3b583bcec7ff06849a0b18d
Alexander Rusinov <Rusinov.a.a@outlook.com>
2020-08-05 15:30:55 +0300

    save

---
+++ charSumm/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace charSumm
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+       
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.Text = "Пример подсчета чисел в строке...";
+        }
+
+
+        string text;
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text.Substring(0, textBox1.Text.Length - 1);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            labelText.Text = textBox1.Text;
+            labelCharCount.Text = textBox1.Text.Length.ToString();
+
+        }
+    }
+}
